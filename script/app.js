4f41904982f6483fc0e96f7b6bc33298900925f9
Clarice Costello <claricostello@gmail.com>
2018-11-10 20:57:36 -0800

    fixed minor typos

--- script/app.js
+++ script/app.js
@@ -5,12 +5,12 @@ function createTable(gridsize) {
 
     //create table, table body head and foot
     var tblEl = document.createElement('table');
-    var theadEL = document.createElement('thead');
+    var theadEl = document.createElement('thead');
     var tbodyEl = document.createElement('tbody');
     var tfootEl = document.createElement('tfoot');
 
     //append table to html, and table body head and foot to table
-    gridEl.appendChild(tblEL);
+    gridEl.appendChild(tblEl);
     tblEl.appendChild(theadEl);
     tblEl.appendChild(tbodyEl);
     tblEl.appendChild(tfootEl);
@@ -18,12 +18,12 @@ function createTable(gridsize) {
     //create table rows and columns, based on gridsize input
     function createRows() {
         // create for loop to make rows
-        for (var i = 0; i < gridsize; i++){
+        for (var i = 0; i < 15; i++){
             var trEl = document.createElement('tr');
             //append rows to body
             tbodyEl.appendChild(trEl);
             //create a loop to populate rows
-            for (var j = 0; j < gridsize; i++){
+            for (var j = 0; j < 15; j++){
                 var tdEl = document.createElement('td');
                 //append cells to rows
                 trEl.appendChild(tdEl);
@@ -31,5 +31,5 @@ function createTable(gridsize) {
         }
     }
     createRows();
-
 }
+createTable(15);
